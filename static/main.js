dceaa26494d3dc94acf00fcb6fd2623da3c9d68c
Hardmath123 <hardmath123@gmail.com>
2016-01-31 11:01:06 -0800

    a fun new demo for the header

--- static/main.js
+++ static/main.js
@@ -1,3 +1,60 @@
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
     document.getElementById("contactemail").href = "mail" + "to" + ":" + "contact" + "@" + "comfortablynumbered.appspotmail.com";
 });
+
+window.addEventListener('load', function() {
+    var box = document.getElementById('header');
+    var can = document.createElement('canvas');
+    can.width = box.clientWidth;
+    can.height = box.clientHeight;
+    box.appendChild(can);
+    var ctx = can.getContext('2d');
+
+    function Ball(ctx) {
+        this.ctx = ctx;
+        this.color = 'hsla(' + Math.floor(360*Math.random()) +', 100%, 50%, 0.5)';
+        this.x = Math.random() * this.ctx.canvas.width;
+        this.y = -Math.random() * this.ctx.canvas.height;
+        this.dx = 2*(Math.random() - 0.5);
+        this.dy = 5*(Math.random() - 0.5);
+    }
+    Ball.prototype.iter = function() {
+        this.x += this.dx;
+        this.y += this.dy;
+        if (this.y > ctx.canvas.height) {
+            this.dy *= -1;
+        } else {
+            this.dy += 0.2;
+        }
+        if (this.x > ctx.canvas.width + 10 || this.x < -10) {
+            this.x = Math.random() * this.ctx.canvas.width;
+            this.y = 0;
+            this.dx = 2*(Math.random() - 0.5);
+            this.dy = 0;
+        }
+    };
+    Ball.prototype.render = function() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, 5, 0, Math.PI*2, false);
+        this.ctx.fill();
+    };
+
+    var balls = [];
+    for (var i=0; i<50; i++) {
+        balls.push(new Ball(ctx));
+    }
+    function loop() {
+        can.width = box.clientWidth;
+        can.height = box.clientHeight;
+        balls.forEach(function(b) {
+            b.iter();
+            b.render();
+        });
+        (window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+         (function(x) {setInterval(x, 1000/60); }))(loop);
+    };
+    loop();
+}, false);
